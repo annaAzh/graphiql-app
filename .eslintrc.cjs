3dfe20813d9f522f7264ac3de233a9ce7f6fb883
Irina Fileva <irinkaskai@gmail.com>
2024-08-22 22:35:22 +0500

    chore: setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
+  ],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', "react-compiler"],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    "react-compiler/react-compiler": "error",
+    "react/react-in-jsx-scope": "off",
+  },
+}
