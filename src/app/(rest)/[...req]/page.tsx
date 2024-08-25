2cd3cf0fcb471e26559d54afc43a4d568008c044
rRedq <38762919+rRedq@users.noreply.github.com>
2024-08-26 02:48:25 +0500

    feat/014: Implement a function call for rest client according to the entered parameters
    * chore: add axios
    
    * feat: add getData function
    
    * feat: implement function call according to the parameters

--- src/app/(rest)/[...req]/page.tsx
+++ src/app/(rest)/[...req]/page.tsx
@@ -1,5 +1,39 @@
-const RestfulPage = async () => {
-  return <div style={{ color: 'red' }}>REST</div>;
+import { ResponseResult } from 'features/ResponseResult';
+import { VALID_METHODS } from 'shared/constants';
+import { getData } from 'shared/lib/api';
+import { Path } from 'shared/types/path';
+import { RestResponse, ValidMethods } from 'shared/types/restful';
+
+const RestfulPage = async ({
+  params,
+  searchParams,
+}: {
+  params: { req: [ValidMethods, string, string] | [ValidMethods, string] };
+  searchParams?: { [key: string]: string };
+}) => {
+  try {
+    const { req } = params;
+    let result: RestResponse | undefined;
+
+    if (![...VALID_METHODS, Path.REST].includes(req[0])) {
+      throw new Error('Invalid required method');
+    }
+
+    switch (req.length) {
+      case 2:
+        result = await getData(req[0], req[1], searchParams);
+        break;
+      case 3:
+        result = await getData(req[0], req[1], searchParams, req[2]);
+        break;
+      default:
+        result = undefined;
+    }
+
+    return result && <ResponseResult data={result} />;
+  } catch {
+    return <>Error</>;
+  }
 };
 
 export default RestfulPage;
