3177c32072ac74114138589ae8436c19de93fb26
Irina Fileva <irinkaskai@gmail.com>
2024-08-28 01:03:11 +0500

    feat: add a language selector

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,13 +1,27 @@
 'use client';
 import { FC } from 'react';
-import Logo from '../../../shared/assets/img/logo.jpg';
-import styles from './Header.module.scss';
 import Image from 'next/image';
+import Logo from 'shared/assets/img/logo.jpg';
+import { MenuItem, Select } from '@mui/material';
+import { Title } from 'shared/components';
+import './Header.scss';
 
 export const Header: FC = () => {
   return (
-    <header className={styles.header}>
-      <Image src={Logo} alt="logo" priority={true} width={210} height={70} />
+    <header className="header">
+      <Image src={Logo} alt="logo" priority width={210} height={65} />
+      <div className="selectorHeader">
+        <Title text={'Language:'} />
+        <Select
+          color="secondary"
+          labelId="select-label"
+          id="select"
+          defaultValue={'en'}
+        >
+          <MenuItem value={'en'}>EN</MenuItem>
+          <MenuItem value={'ru'}>RU</MenuItem>
+        </Select>
+      </div>
     </header>
   );
 };
