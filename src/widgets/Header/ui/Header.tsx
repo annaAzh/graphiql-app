42d4fd9870bad3d21b483455331cd06aba219330
Irina Fileva <irinkaskai@gmail.com>
2024-08-28 01:16:15 +0500

    feat: add click redirects

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -2,14 +2,24 @@
 import { FC } from 'react';
 import Image from 'next/image';
 import Logo from 'shared/assets/img/logo.jpg';
+import { useRouter } from 'next/navigation';
 import { MenuItem, Select } from '@mui/material';
 import { Button, Title } from 'shared/components';
 import './Header.scss';
 
 export const Header: FC = () => {
+  const router = useRouter();
+
   return (
     <header className="header">
-      <Image src={Logo} alt="logo" priority width={210} height={65} />
+      <Image
+        src={Logo}
+        alt="logo"
+        priority
+        width={210}
+        height={65}
+        onClick={() => router.push('/')}
+      />
       <div className="selectorHeader">
         <Title text={'Language:'} />
         <Select
@@ -23,8 +33,12 @@ export const Header: FC = () => {
         </Select>
       </div>
       <div className="containerButtons">
-        <Button size="lg">{'Sign Up'}</Button>
-        <Button size="lg">{'Sign In'}</Button>
+        <Button onClick={() => router.push('/signUp')} size="lg">
+          {'Sign Up'}
+        </Button>
+        <Button onClick={() => router.push('/signIn')} size="lg">
+          {'Sign In'}
+        </Button>
       </div>
     </header>
   );
