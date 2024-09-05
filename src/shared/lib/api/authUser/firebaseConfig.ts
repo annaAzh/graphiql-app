a5607967cc624963f76b97d22a070d4432adfc1c
Irina Fileva <irinkaskai@gmail.com>
2024-09-06 00:22:18 +0500

    feat: add a function to set the user name

--- src/shared/lib/api/authUser/firebaseConfig.ts
+++ src/shared/lib/api/authUser/firebaseConfig.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAGVLcs2N-kQdAhga4JC3_PpsSALXiuIv0',
@@ -15,5 +14,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
-export const db = getFirestore(app);
