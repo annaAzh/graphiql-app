fd023b32c5f231778dea01173685dc0ea969d39f
Irina Fileva <irinkaskai@gmail.com>
2024-09-06 00:28:05 +0500

    feat: add conditions for page privacy in relation to user status

--- src/app/GRAPHQL/layout.tsx
+++ src/app/GRAPHQL/layout.tsx
@@ -1,7 +1,15 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
+import { Path } from 'shared/types/path';
 import { GraphQlPlayground } from 'widgets/GraphQl';
 
 const GraphQLLayout = ({ children }: { children: ReactNode }) => {
+  const cookie = cookies();
+  const user = cookie.get('user');
+
+  if (!user) redirect(Path.MAIN);
+
   return <GraphQlPlayground>{children}</GraphQlPlayground>;
 };
 
