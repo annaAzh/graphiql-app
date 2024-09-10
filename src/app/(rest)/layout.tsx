0fe932fca21e0815f3039aea9a5fbd8fbcaae5b6
Irina Fileva <irinkaskai@gmail.com>
2024-09-10 16:02:53 +0500

    fix: remove redirect terms from pages

--- src/app/(rest)/layout.tsx
+++ src/app/(rest)/layout.tsx
@@ -1,14 +1,7 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
-import { Path } from 'shared/types/path';
 import { Restful } from 'widgets/Restful';
 
 const RestLayout = ({ children }: { children: ReactNode }) => {
-  const cookie = cookies();
-  const user = cookie.get('user');
-
-  if (!user) redirect(Path.MAIN);
 
   return <Restful>{children}</Restful>;
 };
--- src/app/GRAPHQL/layout.tsx
+++ src/app/GRAPHQL/layout.tsx
@@ -1,15 +1,7 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
-import { Path } from 'shared/types/path';
 import { GraphQlPlayground } from 'widgets/GraphQl';
 
 const GraphQLLayout = ({ children }: { children: ReactNode }) => {
-  const cookie = cookies();
-  const user = cookie.get('user');
-
-  if (!user) redirect(Path.MAIN);
-
   return <GraphQlPlayground>{children}</GraphQlPlayground>;
 };
 
