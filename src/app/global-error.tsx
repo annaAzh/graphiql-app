b354638da7c2f1e926c03b14c77ff133f3404493
rRedq <38762919+rRedq@users.noreply.github.com>
2024-09-11 23:17:34 +0500

    feat/008: add error boundary

---
+++ src/app/global-error.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+const GlobalError = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <p>{error.message}</p>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+};
+
+export default GlobalError;
