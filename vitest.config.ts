4c9615a6ccc16cb0fb034763f288fb1fc837283f
rRedq <redq_000@mail.ru>
2024-09-12 13:42:42 +0500

    chore: exclude vitest types

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text'],
       include: ['**/src/**'],
-      exclude: ['**/index.ts'],
+      exclude: ['**/index.ts', '**/src/shared/types/**'],
     },
     globals: true,
     environment: 'jsdom',
