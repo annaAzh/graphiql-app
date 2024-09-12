9609f082b30f753a5a41f4c2676173f568552921
Irina Fileva <irinkaskai@gmail.com>
2024-09-13 00:57:07 +0500

    feat: add translation to the not found page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,11 +1,17 @@
 import { rubik_doodle } from 'shared/styles/fonts/fonts';
+import { cookies } from 'next/headers';
 import style from './styles/Not-found.module.scss';
+import textEN from '../../../locales/en/default.json';
+import textRU from '../../../locales/ru/default.json';
 import clsx from 'clsx';
 
 const NotFound = () => {
+  const cookieStore = cookies();
+  const locale = cookieStore.get('NEXT_LOCALE')?.value;
+
   return (
     <div className={clsx(style.wrapper, rubik_doodle.className)}>
-      {`page hasn't been found`}
+      {locale && locale !== 'en' ? textRU.NotFound : textEN.NotFound}
     </div>
   );
 };
