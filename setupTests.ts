9480c672efb07e31254d5d8ea35802106d5ed65e
rRedq <redq_000@mail.ru>
2024-09-12 16:54:53 +0500

    feat: add rest layout test

--- setupTests.ts
+++ setupTests.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom';
+
+beforeAll(() => {
+  vi.mock('next/font/google', () => ({
+    Rubik: () => ({
+      style: {
+        fontFamily: 'mocked',
+      },
+    }),
+    Rubik_Doodle_Shadow: () => ({
+      style: {
+        fontFamily: 'mocked',
+      },
+    }),
+  }));
+});
---
+++ src/app/(rest)/restLayout.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import RestLayout from './layout';
+import { StoreProvider } from 'core';
+
+vi.mock('next/navigation', async (importOriginal) => {
+  const actual = await importOriginal<typeof import('next/navigation')>();
+  const { useRouter } =
+    await vi.importActual<typeof import('next-router-mock')>(
+      'next-router-mock'
+    );
+  const useParams = vi.fn().mockImplementation(() => {
+    return { req: ['REST', 'test', 'test', 'test'] };
+  });
+  return {
+    ...actual,
+    useRouter: vi.fn().mockImplementation(useRouter),
+    useParams,
+  };
+});
+
+test('a', async () => {
+  const testText = 'testText';
+  const { getByText } = render(
+    <StoreProvider>
+      <RestLayout>{testText}</RestLayout>
+    </StoreProvider>
+  );
+
+  expect(getByText(testText)).toBeInTheDocument();
+
+  screen.debug();
+});
