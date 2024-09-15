95bb41f29393e048f61522ae20ad990a64a3d022
Irina Fileva <irinkaskai@gmail.com>
2024-09-16 00:21:42 +0500

    fix: delete comments

--- src/core/TranslationsProvider/config/i18n.test.ts
+++ src/core/TranslationsProvider/config/i18n.test.ts
@@ -3,20 +3,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 i18n.use(initReactI18next);
-// .init({
-//   lng: 'en',
-//   fallbackLng: 'en',
-//   ns: ['translationsNS'],
-//   defaultNS: 'translationsNS',
-
-//   debug: true,
-
-//   interpolation: {
-//     escapeValue: false,
-//   },
-
-//   resources: { en: { translationsNS: {} } },
-// });
 
 vi.mock('react-i18next', async (importOriginal) => {
   const actual = await importOriginal<typeof import('react-i18next')>();
